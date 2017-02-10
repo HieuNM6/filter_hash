6f4352ad1834a79a37663a812967356ad640f5d9
HieuNM6 <hieunmce@gmail.com>
2017-02-10 08:52:53 +0700

    Refactor json sample to fixtures folder

--- test_filter_nested_hash.rb
+++ test_filter_nested_hash.rb
@@ -14,10 +14,10 @@ class TestFilterNestedHash < Minitest::Test
     private
 
     def json_sample
-        '{"page":{"name":"","url":"","type":"","nodes":{"tag":"div","class":["container"],"children":[{"tag":"div","class":["row"],"children":[{"tag":"form","action":"/api/v1/login","method":"post","children":[{"tag":"label","text":"username","children":[{"tag":"input","type":"text","value":"{{user.username}}"}]},{"tag":"label","text":"password","children":[{"tag":"input","type":"password","value":"{{user.password}}","conditions":[{"condition":"AND","rules":[{"field":"username","type":"string","input":"text","operator":"not","value":"empty"}]}]}]},{"tag":"label","text":"remember password","children":[{"tag":"checkbox","name":"remember_password","value":"{{user.remember}}"}]},{"tag":"div","class":["row"],"children":[{"tag":"button","text":"login","type":"submit"}]}],"privacy":["role a","role b"]}]}]},"ds":{"user":{"type":"query","ds_table":"user","ds_conditions":[{"condition":"AND","rules":[{"field":"id","type":"integer","operator":"gt","value":"100"}]},{"condition":"AND","rules":[{"field":"id","type":"integer","operator":"lt","value":"200"}]}]}}}}'
+        File.read('./fixtures/json_sample.json')
     end
 
     def json_remove_page
-        '{"tag":"div","class":["container"],"children":[{"tag":"div","class":["row"],"children":[{"tag":"form","action":"/api/v1/login","method":"post","children":[{"tag":"label","text":"username","children":[{"tag":"input","type":"text","value":"{{user.username}}"}]},{"tag":"label","text":"password","children":[{"tag":"input","type":"password","value":"{{user.password}}","conditions":[{"condition":"AND","rules":[{"field":"username","type":"string","input":"text","operator":"not","value":"empty"}]}]}]},{"tag":"label","text":"remember password","children":[{"tag":"checkbox","name":"remember_password","value":"{{user.remember}}"}]},{"tag":"div","class":["row"],"children":[{"tag":"button","text":"login","type":"submit"}]}],"privacy":["role a","role b"]}]}]}'
+       File.read('./fixtures/json_remove_page.json')
     end
 end
