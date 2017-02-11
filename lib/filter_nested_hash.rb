3102fc3b65be01464b9027819c3e57629971db35
HieuNM6 <hieunmce@gmail.com>
2017-02-11 09:34:32 +0700

    refactor filter_nested_hash

--- lib/filter_nested_hash.rb
+++ lib/filter_nested_hash.rb
@@ -1,32 +1,37 @@
 require 'oj'
 
 class FilterNestedHash
-  attr_reader :hash
-  def initialize(input_json="")
-      @hash = Oj.load(input_json)
+  attr_accessor :input_hash, :input_tag, :child_tag, :filter_tag, :role
+  def initialize(input_hash, options = {})
+      @input_hash = Oj.load(input_hash)
+      @child_tag = options[:child_tag] || "children"
+      @filter_tag = options[:filter_tag] || "privacy"
+      @role = options[:role] || []
   end
 
-  def base_node
-      @hash["page"]["nodes"]
+  def exec_filter
+    filter(@input_hash)
   end
 
-  def hash_filter(hash, role)
+  private
+
+  def filter(hash)
     hash.each do |k, v|
-      if k == "privacy" && v.is_a?(Array) && !v.empty?
-        if role.any? { |e| v.to_set.include?(e)}
+      if k == @filter_tag && v.is_a?(Array) && !v.empty?
+        if @role.any? { |e| v.to_set.include?(e)}
           return hash
         else
           return nil
         end
-      elsif k == "children" && v.is_a?(Array) && !v.empty?
+      elsif k == @child_tag && v.is_a?(Array) && !v.empty?
         tmp = []
         v.each do |e|
-          tmp_filter = hash_filter(e, role)
+          tmp_filter = filter(e)
           unless tmp_filter.nil?
             tmp.push(tmp_filter)
           end
         end
-        hash["children"] = tmp
+        hash[@child_tag] = tmp
       end
     end
     hash
--- test/test_filter_nested_hash.rb
+++ test/test_filter_nested_hash.rb
@@ -5,27 +5,34 @@ require_relative 'test_helper/test_helper'
 
 class TestFilterNestedHash < Minitest::Test
     def setup
-        @nested_hash = FilterNestedHash.new(json_sample)
+        @nested_hash = FilterNestedHash.new(json_remove_page)
     end
 
     def test_remove_page_info
-        assert_equal @nested_hash.base_node, Oj.load(json_remove_page)
+        assert_equal @nested_hash.input_hash, Oj.load(json_remove_page)
     end
 
     def test_remove_hash_contain_privacy
-        assert_equal @nested_hash.hash_filter(@nested_hash.base_node, ["rolea"]), Oj.load(json_remove_privacy)
+      @nested_hash.role = ["rolea"]
+      assert_equal @nested_hash.exec_filter, Oj.load(json_remove_privacy)
     end
 
     def test_remove_hash_contain_privacy_right
-      assert_equal @nested_hash.hash_filter(Oj.load(json_privacy_with_roleb), ["roleb"]), Oj.load(json_privacy_with_roleb)
+      @nested_hash.input_hash = Oj.load(json_privacy_with_roleb)
+      @nested_hash.role = ["roleb"]
+      assert_equal @nested_hash.exec_filter, Oj.load(json_privacy_with_roleb)
     end
 
     def test_remove_hash_contain_privacy_not_right
-      assert_equal @nested_hash.hash_filter(Oj.load(json_privacy_with_roleb), ["rolec"]), Oj.load(json_remove_privacy)
+      @nested_hash.input_hash = Oj.load(json_privacy_with_roleb)
+      @nested_hash.role = ["rolec"]
+      assert_equal @nested_hash.exec_filter, Oj.load(json_remove_privacy)
     end
 
     def test_remove_hash_contain_privacy_big_file
-      assert_equal @nested_hash.hash_filter(Oj.load(json_big_file), ["roleb"]), Oj.load(json_big_file)
+      @nested_hash.input_hash = Oj.load(json_big_file)
+      @nested_hash.role = ["roleb"]
+      assert_equal @nested_hash.exec_filter, Oj.load(json_big_file)
     end
 
     private
